74311023611d927799b019522c855a9774a60ee4
divyanshusgit <1506divyanshu@gmail.com>
2023-10-05 18:37:25 +0530

    loading the files

---
+++ files/script.js
@@ -0,0 +1,74 @@
+let winSound = new Audio('../sounds/won.mp3');
+let lostSound = new Audio('../sounds/gameOver.mp3');
+
+let a = 0;
+let b = 10;
+let ran = (a + 1) + Math.floor((b - (a + 1)) * Math.random());
+
+let input = document.getElementById('input');
+input.addEventListener('keydown', keyDownFunc);
+let allowedStr = '0123456789';
+let allowedArr = Array.from(allowedStr);
+function keyDownFunc(a) {
+    if (a.key != 'Backspace') {  
+        if ((input.value.length >= 1) || !(allowedArr.includes(a.key))) { 
+            a.preventDefault();
+        }
+    }
+}
+
+let message = document.getElementById('message');
+
+let chanceContent = document.querySelector('.chance-content');
+
+let counter = 0;
+
+let check = document.getElementById('check');
+function gameOver() {
+    check.innerHTML = `Replay`;
+    check.onclick = replayFunc;
+    function replayFunc(elem) {
+        location.reload();  
+    }
+    input.setAttribute('disabled', 'true'); 
+    input.style.cursor = 'not-allowed';
+}
+check.onclick = submitFunc;
+function submitFunc() {
+    counter++; 
+    document.getElementById(`color${6 - counter}`).style.backgroundColor = `rgb(0,183,255)`; 
+    chanceContent.innerText = `${5 - counter} chances left`; 
+    if (counter == 5 && parseInt(input.value) != ran) { 
+        lostSound.play(); 
+        setTimeout(function () {
+            lostSound.pause(); 
+        }, 400);
+        message.setAttribute('style', 'background-color: red; text-shadow: 2px 2px darkred;'); 
+        message.innerHTML = `Game Over &#128551`; 
+        message.style.visibility = 'visible';
+        gameOver(); 
+    }
+
+    else {
+        if (parseInt(input.value) == ran) {
+            winSound.play(); 
+            setTimeout(function () {
+                winSound.pause(); 
+            }, 1390)
+            message.setAttribute('style', 'background-color: lightgreen; text-shadow: 2px 2px green;'); 
+            message.innerHTML = `You got it! &#129395;`; 
+            message.style.visibility = 'visible';
+            gameOver(); 
+        }
+        else {
+            message.setAttribute('style', 'background-color: orange; text-shadow: 2px 2px red;'); 
+            if (parseInt(input.value) > ran) {
+                message.innerHTML = `it's smaller &#129300;`
+            }
+            else {
+                message.innerHTML = `it's greater &#129300;`
+            }
+            message.style.visibility = 'visible';
+        }
+    }
+}
